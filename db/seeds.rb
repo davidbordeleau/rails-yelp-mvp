6158b88f8b4ceee5920af7673e10f0c690b4cf21
David Bordeleau <davidbordeleau@hotmail.com>
2019-02-20 15:49:50 +0100

    done fonctionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'faker'
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+TYPES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+10.times do
+  Restaurant.create(name: Faker::Games::Zelda.game, address: Faker::Games::Zelda.location, category: TYPES[rand(0..4)])
+end
